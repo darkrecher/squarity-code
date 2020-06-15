7a86279dddc94d1213836ceed930ed499f54d09a
darkrecher <recher_burp@yahoo.fr>
2020-06-16 00:06:03 +0200

    Création d'une nouvelle classe en TypeScript. On finit par y arriver.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,12 @@
 import Vue from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 import GameBoard from './components/GameBoard.vue'
+import BoardModel from './classes/BoardModel'
+
+const bm = new BoardModel()
+const x = bm.getTheA() + bm.testVetur()
+// console.log(bm.getTheA())
+console.log(x)
 
 export default Vue.extend({
   name: 'App',
---
+++ src/classes/BoardModel.ts
@@ -0,0 +1,21 @@
+class BoardModel {
+  private a: number
+
+  constructor () {
+    this.a = 7
+  }
+
+  getA () {
+    return this.a
+  }
+
+  getTheA () {
+    return this.a * 2
+  }
+
+  testVetur () {
+    return 'pouet'
+  }
+}
+
+export default BoardModel
