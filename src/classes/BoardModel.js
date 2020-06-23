c06451fc08b013734efb86e74b2f15a15ddd6eaf
darkrecher <recher_burp@yahoo.fr>
2020-06-24 00:19:24 +0200

    Début de dessin d'une aire de jeu, avec un tileset

--- src/classes/BoardModel.js
+++ src/classes/BoardModel.js
@@ -1,6 +1,6 @@
 class BoardModel {
 
-  constructor (width = 12, height = 8) {
+  constructor (width = 20, height = 14) {
     this.w = width
     this.h = height
 
@@ -8,9 +8,17 @@ class BoardModel {
     for (let y = 0; y < this.h; y++) {
       this.tiles[y] = []
       for (let x = 0; x < this.w; x++) {
-        this.tiles[y][x] = (x + y) % 2
+        this.tiles[y][x] = ' '
       }
     }
+    // TODO : c'est moche, mais c'est pas grave.
+    this.tiles[7] =  ' 70000001           '.split('')
+    this.tiles[8] =  ' 588888881          '.split('')
+    this.tiles[9] =  '  688888881         '.split('')
+    this.tiles[10] = '  6888888881        '.split('')
+    this.tiles[11] = ' 78888888882        '.split('')
+    this.tiles[12] = ' 54888888843        '.split('')
+    this.tiles[13] = '   5444443          '.split('')
   }
 
   getTile (x, y) {
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="gameboard">
-    <p>{{ msg}}</p>
-    <p>{{ message }}</p>
+    <p>{{ msg}} - {{ message }}</p>
 
     <canvas id="c"></canvas>
     <div>
@@ -9,6 +8,12 @@
       <button @click="subWidth">-</button>
       <button @click="addWidth">+</button>
     </div>
+
+    <p>
+      Tileset créé par Buch :
+      <br>
+      <a href="https://opengameart.org/users/buch">https://opengameart.org/users/buch</a>
+    </p>
 
   </div>
 </template>
@@ -42,9 +47,20 @@ export default {
       ctxCanvas: null,
       boardModel: new BoardModel(),
       rectWidth: 200,
-      tileWidth: 40,
-      tileHeight: 40,
-      oneImage: null
+      tileWidth: 32,
+      tileHeight: 32,
+      tileAtlas: null,
+      coordImgFromData: {
+        '0': [48, 32],
+        '1': [96, 32],
+        '2': [96, 48],
+        '3': [96, 96],
+        '4': [48, 96],
+        '5': [32, 96],
+        '6': [32, 48],
+        '7': [32, 32],
+        '8': [48, 48  ],
+      }
     }
   },
 
@@ -57,7 +73,7 @@ export default {
     // TODO : il faudra quand même l'adapter à la taille de la fenêtre,
     //        et aux proportions du BoardModel aussi.
     nodeCanvas.width = 600
-    nodeCanvas.height = 400
+    nodeCanvas.height = 448
     this.drawRect()
   },
 
@@ -67,22 +83,19 @@ export default {
 
       // clear canvas
       this.ctxCanvas.fillStyle = '#000000'
-      this.ctxCanvas.fillRect(0, 0, 600, 400)
+      this.ctxCanvas.fillRect(0, 0, 640, 448)
       let canvasX = 0
       let canvasY = 0
 
-      this.oneImage = await loadImage(require('../assets/logo.png'))
+      this.tileAtlas = await loadImage(require('../assets/dungeon_tiles_2.png'))
 
       for (let y = 0; y < this.boardModel.h; y++) {
         for (let x = 0; x < this.boardModel.w; x++) {
-          if (this.boardModel.getTile(x, y)) {
-            this.ctxCanvas.fillStyle = '#007000'
-          } else {
-            this.ctxCanvas.fillStyle = '#B00000'
-          }
-          this.ctxCanvas.fillRect(canvasX, canvasY, this.tileWidth, this.tileHeight)
-          if (this.boardModel.getTile(x, y)) {
-            this.ctxCanvas.drawImage(this.oneImage, canvasX, canvasY, this.tileWidth, this.tileHeight)
+          let tileData = this.boardModel.getTile(x, y)
+          if (tileData != ' ') {
+            let coordImg = this.coordImgFromData[tileData]
+            // console.log("coordImg", coordImg)
+            this.ctxCanvas.drawImage(this.tileAtlas, coordImg[0], coordImg[1], 16, 16, canvasX, canvasY, this.tileWidth, this.tileHeight)
           }
           canvasX += this.tileWidth
         }
