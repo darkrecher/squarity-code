7515b89489e89317a2b0f072357b3d5d1f2b2813
darkrecher <r@recher.recher>
2025-07-12 11:58:06 +0200

    CSS pas trop dégueu pour les pages de documentation.

--- src/components/docarticles/MainDocV2Toc.vue
+++ src/components/docarticles/MainDocV2Toc.vue
@@ -1,27 +1,33 @@
 <template>
-  <a href="#doc-start"><h1>Documentation de référence de Squarity V2</h1></a>
-  <h2>Le tileset <a class="header-anchor" href="#le-tileset">¶</a></h2>
-  <h2>Configuration json <a class="header-anchor" href="#configuration-json">¶</a></h2>
-  <h2>Notions de base du &quot;game code&quot; <a class="header-anchor" href="#notions-de-base-du-game-code">¶</a></h2>
-  <a href="#pouet"><h3>blaaa</h3></a>
-  <a href="#pouet"><h4>blaaa.1</h4></a>
-  <a href="#pouet"><h4>blaaa.2</h4></a>
-  <a href="#pouet"><h3>blou</h3></a>
-  <a href="#pouet"><h3>blououuuui</h3></a>
-  <a href="#schéma-daffichage-calculs-des-tailles"><h2>Schéma d'affichage, calculs des tailles</h2></a>
-  <h2>class Direction <a class="header-anchor" href="#class-direction">¶</a></h2>
-  <h2>class Coord <a class="header-anchor" href="#class-coord">¶</a></h2>
-  <h2>class Rect <a class="header-anchor" href="#class-rect">¶</a></h2>
-  <h2>class GameObject <a class="header-anchor" href="#class-gameobject">¶</a></h2>
-  <h2>class Layer <a class="header-anchor" href="#class-layer">¶</a></h2>
-  <h2>class GameModel <a class="header-anchor" href="#class-gamemodel">¶</a></h2>
-  <h2>class EventResult <a class="header-anchor" href="#class-eventresult">¶</a></h2>
-  <h2>Transitions <a class="header-anchor" href="#transitions">¶</a></h2>
-  <h2>Info supplémentaires pour les sprites <a class="header-anchor" href="#info-supplémentaires-pour-les-sprites">¶</a></h2>
-  <h2>class ComponentImageModifierDeMesFessesSurLaCommode <a class="header-anchor" href="#class-componentimagemodifier">¶</a></h2>
-  <h2>class ComponentBackCaller <a class="header-anchor" href="#class-componentbackcaller">¶</a></h2>
-  <h2>Itérer dans l'aire de jeu <a class="header-anchor" href="#itérer-dans-laire-de-jeu">¶</a></h2>
-  <h2>Exemple bonus : afficher tous les sprites existants <a class="header-anchor" href="#exemple-bonus--afficher-tous-les-sprites-existants">¶</a></h2>
+  <div class="doc-toc">
+    <!--
+      TODO: il faut imbriquer les titres dans des divs.
+      Comme ça on pourrait faire du CSS plus sympa avec des décalages, des cadres imbriqués, etc.
+    -->
+    <a href="#doc-start"><h1>Documentation de référence de Squarity V2</h1></a>
+    <h2>Le tileset <a class="header-anchor" href="#le-tileset">¶</a></h2>
+    <h2>Configuration json <a class="header-anchor" href="#configuration-json">¶</a></h2>
+    <h2>Notions de base du &quot;game code&quot; <a class="header-anchor" href="#notions-de-base-du-game-code">¶</a></h2>
+    <a href="#pouet"><h3>blaaa</h3></a>
+    <a href="#pouet"><h4>blaaa.1</h4></a>
+    <a href="#pouet"><h4>blaaa.2</h4></a>
+    <a href="#pouet"><h3>blou</h3></a>
+    <a href="#pouet"><h3>blououuuui</h3></a>
+    <a href="#schéma-daffichage-calculs-des-tailles"><h2>Schéma d'affichage, calculs des tailles</h2></a>
+    <h2>class Direction <a class="header-anchor" href="#class-direction">¶</a></h2>
+    <h2>class Coord <a class="header-anchor" href="#class-coord">¶</a></h2>
+    <h2>class Rect <a class="header-anchor" href="#class-rect">¶</a></h2>
+    <h2>class GameObject <a class="header-anchor" href="#class-gameobject">¶</a></h2>
+    <h2>class Layer <a class="header-anchor" href="#class-layer">¶</a></h2>
+    <h2>class GameModel <a class="header-anchor" href="#class-gamemodel">¶</a></h2>
+    <h2>class EventResult <a class="header-anchor" href="#class-eventresult">¶</a></h2>
+    <h2>Transitions <a class="header-anchor" href="#transitions">¶</a></h2>
+    <h2>Info supplémentaires pour les sprites <a class="header-anchor" href="#info-supplémentaires-pour-les-sprites">¶</a></h2>
+    <h2>class ComponentImageModifierDeMesFessesSurLaCommode <a class="header-anchor" href="#class-componentimagemodifier">¶</a></h2>
+    <h2>class ComponentBackCaller <a class="header-anchor" href="#class-componentbackcaller">¶</a></h2>
+    <h2>Itérer dans l'aire de jeu <a class="header-anchor" href="#itérer-dans-laire-de-jeu">¶</a></h2>
+    <h2>Exemple bonus : afficher tous les sprites existants <a class="header-anchor" href="#exemple-bonus--afficher-tous-les-sprites-existants">¶</a></h2>
+  </div>
 </template>
 
 
--- src/views/DocArticleView.vue
+++ src/views/DocArticleView.vue
@@ -7,6 +7,13 @@
       <div class="content">
         <div id="doc-start"></div>
         <HeaderCreate/>
+        <div class="table-of-content-narrow" id="toc-narrow">
+          <!--
+            TODO: ce serait bien d'arriver à écrire une seule fois ce component.
+            Au lieu de l'avoir deux fois, et il y en a toujours un sur deux qui est invisible.
+          -->
+          <MainDocV2Toc/>
+        </div>
         <MainDocV2/>
       </div>
     </div>
@@ -14,10 +21,46 @@
   </main>
 </template>
 
-<script setup>
-  import MainDocV2 from '@/components/docarticles/MainDocV2.vue'
-  import MainDocV2Toc from '@/components/docarticles/MainDocV2Toc.vue'
-  import HeaderCreate from '@/components/HeaderCreate.vue'
+<script>
+
+import MainDocV2 from '@/components/docarticles/MainDocV2.vue'
+import MainDocV2Toc from '@/components/docarticles/MainDocV2Toc.vue'
+import HeaderCreate from '@/components/HeaderCreate.vue'
+
+export default {
+  name: 'DocArticleView',
+  components: {
+    MainDocV2,
+    MainDocV2Toc,
+    HeaderCreate,
+  },
+
+  props: {},
+
+  async mounted() {
+    // https://css-tricks.com/styling-based-on-scroll-position/
+    if (
+      "IntersectionObserver" in window &&
+      "IntersectionObserverEntry" in window &&
+      "intersectionRatio" in window.IntersectionObserverEntry.prototype
+    ) {
+      let observer = new IntersectionObserver(entries => {
+        //console.log(entries[0].boundingClientRect.y);
+        //console.log(entries[0].boundingClientRect);
+        if (entries[0].boundingClientRect.bottom > 0) {
+          console.log("Il faut masquer le bouton 'go to the top'.")
+          // document.body.classList.add("header-not-at-top");
+        } else {
+          console.log("Il faut montrer le bouton 'go to the top'.")
+          // document.body.classList.remove("header-not-at-top");
+        }
+      });
+      observer.observe(document.querySelector("#toc-narrow"));
+    }
+  },
+
+}
+
 </script>
 
 
@@ -28,10 +71,14 @@
   left: 0;
   top: 0;
   height: 100vh;
-  background: #202020;
   border-right: 1px solid #ccc;
+  background: #202020;
   overflow-y: auto;
   padding: 0.2em;
+}
+
+.table-of-content-narrow {
+  width: 100%;
 }
 
 .content {
@@ -39,13 +86,15 @@
   max-width: 80em;
 }
 
-@media (max-width: 960px) {
+@media (max-width: 959px) {
   .table-of-content {
-    width: 200px;
+    visibility: hidden;
+  }
+  .table-of-content-narrow {
+    visibility: visible;
   }
   .content {
-    /* Il faut que ce soit pareil que table-of-content.width */
-    margin-left: 200px;
+    margin-left: 0px;
     padding: 0.1em;
   }
 }
@@ -54,7 +103,12 @@
   .table-of-content {
     width: 300px;
   }
+  .table-of-content-narrow {
+    visibility: hidden;
+    height: 0;
+  }
   .content {
+    /* Il faut que ce soit pareil que table-of-content.width */
     margin-left: 300px;
     padding: 0.2em 2em 0.2em 2em;
   }
@@ -64,6 +118,10 @@
   .table-of-content {
     width: 500px;
     padding: 0.5em 2em 0.5em 1em;
+  }
+  .table-of-content-narrow {
+    visibility: hidden;
+    height: 0;
   }
   .content {
     margin-left: 500px;
